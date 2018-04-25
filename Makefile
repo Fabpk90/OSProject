042a80849c90e78981189028e4b9709699ca7bae
Santoro Fabrizio <fabrizio1996@live.fr>
2018-04-25 16:07:57 +0200

    PRotecting against yaspr hacking

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ clean:
 	make run
 
 run: compile
-	./BlackJack.paf
+	./BlackJack.paf config.dat
 
 compile:
 	gcc -g3 -Wall Util/fileHandler.c Util/cardHandler.c Actor/bank.c Util/deck.c Actor/player.c main.c -pthread -o BlackJack.paf
--- main.c
+++ main.c
@@ -14,49 +14,52 @@ int main(int argc, char **argv)
   player_t * players = NULL;
   bank_t * bank = NULL;
   int i = 0;
+  int load = 0;
 
-  int load = initGame("config.dat", &bank, &players);
-
-  if(load == 0)
+  if(argc == 2)
   {
-    threads = malloc(sizeof(pthread_t) * bank->nbPlayer);
+    load = initGame(argv[1], &bank, &players);
 
-    for(i = 0; i < bank->nbPlayer; i++)
+    if(load == 0)
     {
-      pthread_create(&threads[i], NULL, playerManager, &players[i]);
+      threads = malloc(sizeof(pthread_t) * bank->nbPlayer);
+
+      for(i = 0; i < bank->nbPlayer; i++)
+      {
+        pthread_create(&threads[i], NULL, playerManager, &players[i]);
+      }
+
+      bankManager(bank, threads, players);
+
+      for(i = 0; i < bank->nbPlayer; i++)
+      {
+          pthread_join(threads[i], NULL);
+      }
     }
-
-    bankManager(bank, threads, players);
-
-    for(i = 0; i < bank->nbPlayer; i++)
+    else if(load == ERROR_FILE_OPEN)
     {
-        pthread_join(threads[i], NULL);
+      printf("Erreur d'ouverture du fichier de config\n");
+    }
+    else if(load == ERROR_FILE_READ)
+    {
+      printf("Erreur de lecture du fichier\n");
+    }
+    else
+    {
+      printf("Erreur de parsing du fichier\n");
+
+      free(players);
+      free(threads);
+      pthread_barrier_destroy(bank->barrierRound);
+      pthread_barrier_destroy(bank->barrierCard);
+      pthread_barrier_destroy(bank->barrierRoundTmp);
+      pthread_barrier_destroy(bank->barrierCardTmp);
+      free(bank->barrierRound);
+      free(bank->barrierRoundTmp);
+      free(bank->barrierCard);
+      free(bank->barrierCardTmp);
+      free(bank);
     }
   }
-  else if(load == ERROR_FILE_OPEN)
-  {
-    printf("Erreur d'ouverture du fichier de config\n");
-  }
-  else if(load == ERROR_FILE_READ)
-  {
-    printf("Erreur de lecture du fichier\n");
-  }
-  else
-  {
-    printf("Erreur de parsing du fichier\n");
-  }
-
-  free(players);
-  free(threads);
-  pthread_barrier_destroy(bank->barrierRound);
-  pthread_barrier_destroy(bank->barrierCard);
-  pthread_barrier_destroy(bank->barrierRoundTmp);
-  pthread_barrier_destroy(bank->barrierCardTmp);
-  free(bank->barrierRound);
-  free(bank->barrierRoundTmp);
-  free(bank->barrierCard);
-  free(bank->barrierCardTmp);
-  free(bank);
-
   return 0;
 }
